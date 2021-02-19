72943c8c8733475f60349384305279e60e02ee55
Elian_Estrada <chictibiris@gmail.com>
2021-02-19 16:42:58 -0600

    first commit

---
+++ src/project1/Project1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+import com.elian_estrada.gui.Home;
+/**
+ *
+ * @author elian_estrada
+ */
+public class Project1 {
+
+    private static Home home = new Home();
+    
+    public static void main(String[] args) {
+        
+        home.setVisible(true);
+        home.setLocationRelativeTo(null);
+        
+    }
+    
+}
